6ada63c3d7c22174fbfc1856ae215e150a23fe49
Padmaashini Sukumaran <padmaashini.s@gmail.com>
2021-02-16 08:54:18 -0500

    added preview collections and sign-in component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+import { Switch, Route} from 'react-router-dom';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react'; 
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`} >
+const MenuItem = ({ title, imageUrl, size, history, location, linkUrl, match }) => (
+    <div className={`${size} menu-item`}  onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        {console.log(location)}
         <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className = 'content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
@@ -12,4 +14,4 @@ const MenuItem = ({title, imageUrl, size}) => (
     </div>
 )
 
-export default MenuItem; 
+export default withRouter(MenuItem); 
