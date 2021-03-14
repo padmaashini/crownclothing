48decc7c4c344197bac8fb132afd535931115311
Padmaashini Sukumaran <padmaashini.s@gmail.com>
2021-03-14 18:07:01 -0400

    setup redux and set current user in redux store

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { connect } from 'react-redux';
 import CollectionItem from '../collection-item/collection-item.component';
 
 const Header = ({currentUser}) => (
@@ -29,4 +30,8 @@ const Header = ({currentUser}) => (
     </div>
 )
 
-export default Header; 
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header); 
