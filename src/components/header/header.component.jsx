c57b496b3a28cd3886686ea8c11651c0fa34a341
Padmaashini Sukumaran <padmaashini.s@gmail.com>
2021-03-14 21:05:15 -0400

    set up redux type files, and added cart dropdown toggle redux state

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,10 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { connect } from 'react-redux';
 import CollectionItem from '../collection-item/collection-item.component';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -24,14 +26,19 @@ const Header = ({currentUser}) => (
                 </div>
                 :
                 <Link className='option' to='/signin'>SIGN IN</Link>
-
             }
+            <CartIcon/>
         </div>
+        {
+            hidden ? null : <CartDropdown/>
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
+
 })
 
 export default connect(mapStateToProps)(Header); 
